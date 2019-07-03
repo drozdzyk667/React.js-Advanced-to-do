28a6598c7b5bbda0c0e113bf42851592e9cafd1e
drozdzyk667 <drozdzyk92@gmail.com>
2019-07-03 21:59:18 +0200

    Updates

---
+++ precache-manifest.bceda341cc9ab34183c3c033c7b129a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f38a5308e600d32f00b2e60d2448c83",
+    "url": "/React.js-scrum-board/index.html"
+  },
+  {
+    "revision": "386b54ed92c160c33e71",
+    "url": "/React.js-scrum-board/static/css/main.1378b886.chunk.css"
+  },
+  {
+    "revision": "ed7f92c0006dbc7ca91c",
+    "url": "/React.js-scrum-board/static/js/2.2cabf52f.chunk.js"
+  },
+  {
+    "revision": "386b54ed92c160c33e71",
+    "url": "/React.js-scrum-board/static/js/main.7a104e7a.chunk.js"
+  },
+  {
+    "revision": "3c397747ebc2920cddb9",
+    "url": "/React.js-scrum-board/static/js/runtime~main.04017835.js"
+  }
+]);
